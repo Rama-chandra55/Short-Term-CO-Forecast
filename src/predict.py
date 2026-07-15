27c67a3297539d06f61ba70ff5c7ee1fe69bedaf
Rama-chandra55 <rock6262712@gmail.com>
2026-07-15 11:09:11 +0530

    fix: Add sys.path.append to fix ModuleNotFoundError when running scripts

--- src/predict.py
+++ src/predict.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import tensorflow as tf
 from src.model import AttentionLayer
 from src.preprocess import preprocess_pipeline
